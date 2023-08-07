cdbef304ed3e3b4ec09d3b077429364795a8a61b
Gabriel <gabrieltohouenou@gmail.com>
2023-08-07 10:46:07 +0100

    adding to timers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,45 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { count, interval, map, Observable } from 'rxjs'
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: '/app.component.html',
-  styleUrls: ['/app.component.css'],
-  animations: [
-    // animation triggers ...
-  ]
+    selector: 'app-root',
+    templateUrl: '/app.component.html',
+    styleUrls: ['/app.component.css'],
+    animations: [
+        // animation triggers ...
+    ]
 })
-export class AppComponent {
-  title = 'app-music';
-  receivedText: string | undefined;
+export class AppComponent implements OnInit{
+    title = 'app-music';
+    receivedText: string | undefined;
+    timerObservable!: Observable<string>;
+    count!: string;
 
-  parentReceive($event: string) {
-    // console.log('parent: '+ $event);
-    this.receivedText = $event;
+    constructor() {}
+
+    ngOnInit(): void {
+        this.timerObservable = interval(1000).pipe(
+            map(num => {
+                const hours = Math.floor(num / 3600);
+                const minutes = Math.floor(num / 60);
+                return `${this.format(hours)} h ${this.format(minutes - hours * 60)} min ${this.format(num - minutes * 60)}s`; 
+            })
+        )
+        this.timerObservable.subscribe(num => {
+            this.count = num;
+        });
+    }
     
-}
+    format(num: number): string {
+        return (num < 10 ? '0' : ' ' )+ num;
+    }
+
+    parentReceive($event: string) {
+        // console.log('parent: '+ $event);
+        this.receivedText = $event;
+    }
+    
+
+
 }
